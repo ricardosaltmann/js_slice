cc1704f27b42d669aff5f294e9b601d8f7c3e400
Ricardo <ricardoaltmann@outlook.com.br>
2022-01-18 16:57:47 -0300

    slice

---
+++ slice.js
@@ -0,0 +1,11 @@
+const nomes = ['João', 'Juliana', 'Ana', 'caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius',
+ 'Rhenan', 'Renata', 'Dayse', 'Camilo' ]
+
+ const sala1 = nomes.slice(0, nomes.length/2)
+
+ const sala2 = nomes.slice(nomes.length/2)
+
+ console.log(sala1)
+ console.log(sala2)
+
+ 
